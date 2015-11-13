1eff99ab7dc2b0bdd0f68c299ffb39e5c688de25
Kyle Naizer <kyle.naizer@gmail.com>
2015-11-13 04:31:01 +0000

    Built a model test on quote author.

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "unique_tag" do
+  quote = Quote.create(:author => 'Kyle Naizer', :saying => 'The beauty of travel happens when you least expect it.')
+  expected = 'KN#' + quote.id.to_s
+  actual = quote.unique_tag
+  assert_equal expected, actual
   end
 end
